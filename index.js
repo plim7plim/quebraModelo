4a601a9e9fc8f89c7e5631f9fb1ebe3cceef2f96
Plínio Peixoto Dos Santos <167206965+plim7plim@users.noreply.github.com>
2026-03-09 14:49:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function calcular(){
+function calcular() {
+    let pesoNota = Number(document.getElementById("pesoNota").value);
+    let pesoFinal = Number(document.getElementById("pesoFinal").value);
 
-let pesoNota = Number(document.getElementById("pesoNota").value);
-let pesoFinal = Number(document.getElementById("pesoFinal").value);
+    let tolerancia = pesoNota * 0.0025;
+    let quebra = pesoNota - pesoFinal;
+    let percentual = (quebra / pesoNota) * 100;
+    let excesso = Math.abs(quebra) - tolerancia;
+    let resultado = document.getElementById("resultado");
 
-let tolerancia = pesoNota * 0.0025;
-
-let quebra = pesoNota - pesoFinal;
-let percentual = (quebra / pesoNota) * 100;
-
-let excesso = Math.abs(quebra) - tolerancia;
-
-let resultado = document.getElementById("resultado");
-
-if (pesoFinal > pesoNota + tolerancia || pesoFinal < pesoNota - tolerancia){
-resultado.innerText =
-`Houve uma quebra de ${quebra.toFixed(2)} kg.
-Poderia dar uma diferença de no máximo ${tolerancia.toFixed(2)} kg.
-A diferença acima do permitido foi ${excesso.toFixed(2)} kg.
-Isso representa ${percentual.toFixed(2)}% da carga.`;
-}else{
-resultado.innerText = "Peso dentro da tolerância.";
+    if (pesoFinal > pesoNota + tolerancia || pesoFinal < pesoNota - tolerancia) {
+        resultado.innerText =
+        `Houve uma quebra de ${quebra.toFixed(2)} kg.
+    Poderia dar uma diferença de no máximo ${tolerancia.toFixed(2)} kg.
+    A diferença acima do permitido foi ${excesso.toFixed(2)} kg.
+    Isso representa ${percentual.toFixed(2)}% da carga.`;
+    } else {
+        resultado.innerText = "Peso dentro da tolerância.";
+    }
 }
-} 
 
-function reiniciar(){
-document.getElementById("formulario").reset();
-document.getElementById("resultado").innerText = "";
+function reiniciar() {
+    document.getElementById("formulario").reset();
+    document.getElementById("resultado").innerText = "";
 }
+
+window.calcular = calcular;
+window.reiniciar = reiniciar;
